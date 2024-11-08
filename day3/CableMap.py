92d99a72924f48a75e40766120a79c56217c1482
Martin van Diest <martin.van.diest@intemo.com>
2024-11-08 11:58:41 +0100

    Finished day1 but performance issues with big matrix
    Try on own laptop

--- day3/CableMap.py
+++ day3/CableMap.py
@@ -11,19 +11,50 @@ class CableMap(Matrix):
     }
     def __init__(self, dimj, dimi):
         super().__init__(dimj, dimi, '.')
-        self.start = (math.floor(dimj / 2), math.floor(dimi // 2))
-        print(self.grid)
+        self.start = (math.floor(dimj / 2), math.floor(dimi / 2))
+        self.crossings = []
 
+    def process_data_lines(self, datalines):
+        lines = datalines.split("\n")
+        for index, line in enumerate(lines):
+            wire = index + 1  # wire 1 = 1, wire 2 = 2
+            datas = line.strip().split(",")
             j, i = self.start
-
+            for dirs in datas:
+                dr = dirs[0]
+                unts = int("".join(dirs[1:]))
+                j, i = self.do_dirs(dr, unts, j ,i , wire)
+        
 
     def do_dirs(self, dr, unts, j, i, wire):
-        for loop over unts
-        newj, newi = j + (self.DIRECTION[dr][0] * unts), i + self.DIRECTION[dr][1]
-        if self.grid[newj][newi] == '.' or self.grid[newj][newi] == 'o':
-            self.grid[newj][newi] = f"{wire}"
-        elif self.grid[newj][newi] == '1' or self.grid[newj][newi] == '2':
-            self.grid[newj][newi] = 'X'
-        elif self.grid[newj][newi] == 'X':
-            self.grid[newj][newi] = 'X'
+        # for loop over unts
+        print(f"moving {unts} units to {dr}.")
+        for n in range(unts + 1):
+            newj, newi = j + (self.DIRS[dr][0] * n), i + (self.DIRS[dr][1] * n)
+            otherwire = 2 if wire == 1 else 1
+            if self.grid[newj][newi] == '.':
+                self.grid[newj][newi] = f"{wire}"
+            elif self.grid[newj][newi] == f"{otherwire}":
+                self.grid[newj][newi] = 'X'
+                if newj != self.start[0] and newi != self.start[1]:
+                    self.crossings.append((newj, newi))
         return newj, newi
+
+
+    def _manhattan_distance(self, coords):
+        x = abs(self.start[0] - coords[0])
+        y = abs(self.start[1] - coords[1])
+        return x + y
+
+
+    def shortest_manhattan_distance(self):
+        shortest = {
+            'dist': 1000000000,
+            'coordinates': (-1, -1),
+        }
+        for crd in self.crossings:
+            print(f"{crd} -> {self._manhattan_distance(crd)}")
+            if self._manhattan_distance(crd) < shortest['dist']:
+                shortest['dist'] = self._manhattan_distance(crd)
+                shortest['coordinates'] = crd
+        return shortest
--- day3/day3.py
+++ day3/day3.py
@@ -3,18 +3,23 @@ from AoC19.day3.CableMap import CableMap
 
 datalines = read_data("day3/input_day3.txt")
 
+datal = "\n".join(datalines)
+
 data = """R75,D30,R83,U83,L12,D49,R71,U7,L72
 U62,R66,U55,R34,D71,R55,D58,R83"""  # = distance 159
 data2 = """R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
 U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"""  # = distance 135
+
+testdata = """R8,U5,L5,D3
+U7,R6,D4,L4"""
     
 def part1():
     # Your code for part 1 goes here
-    print(data)
-    m = CableMap(100, 100)
-    m.do_data_lines(data)
-
+    m = CableMap(35000, 35000)
+    m.process_data_lines(datal)
     print(m)
+    print(m.crossings)
+    print(m.shortest_manhattan_distance())
 
     
         
