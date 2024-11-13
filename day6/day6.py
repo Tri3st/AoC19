435b86ab605ad8e861783a4f8a6170f727915de0
Martin van Diest <martin.van.diest@gmail.com>
2024-11-13 19:14:17 +0100

    Make a start with day 6

--- day6/day6.py
+++ day6/day6.py
@@ -2,7 +2,19 @@ from MyMods.ReadDataFile import read_data
         
 datalines = read_data("day6/input_day6.txt")
 
-    
+data = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L"""
+
+
 def part1():
     # Your code for part 1 goes here
     pass
